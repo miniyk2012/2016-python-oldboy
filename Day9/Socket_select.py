873820e30aeb834b6a95bae66259506955436097
杨恺 <yangk@ersoft.cn>
2016-05-06 09:24:39 +0800

    早上视频学习

---
+++ Day9/Socket_select.py
@@ -0,0 +1,86 @@
+#_*_coding:utf-8_*_
+__author__ = 'Alex Li'
+ 
+import select
+import socket
+import sys
+import queue
+ 
+# Create a TCP/IP socket
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.setblocking(False)
+ 
+# Bind the socket to the port
+server_address = ('localhost', 10000)
+print(sys.stderr, 'starting up on %s port %s' % server_address)
+server.bind(server_address)
+ 
+# Listen for incoming connections
+server.listen(5)
+ 
+# Sockets from which we expect to read
+inputs = [ server ]
+ 
+# Sockets to which we expect to write
+outputs = [ ]
+ 
+message_queues = {}
+while inputs:
+ 
+    # Wait for at least one of the sockets to be ready for processing
+    print( '\nwaiting for the next event')
+    readable, writable, exceptional = select.select(inputs, outputs, inputs, 2)
+    # Handle inputs
+    for s in readable:
+ 
+        if s is server:
+            # A "readable" server socket is ready to accept a connection
+            connection, client_address = s.accept()
+            print('new connection from', client_address)
+            connection.setblocking(False)
+            inputs.append(connection)
+ 
+            # Give the connection a queue for data we want to send
+            message_queues[connection] = queue.Queue()
+        else:
+            data = s.recv(1024)
+            if data:
+                # A readable client socket has data
+                print(sys.stderr, 'received "%s" from %s' % (data, s.getpeername()) )
+                message_queues[s].put(data)
+                # Add output channel for response
+                if s not in outputs:
+                    outputs.append(s)
+            else:
+                # Interpret empty result as closed connection
+                print('closing', client_address, 'after reading no data')
+                # Stop listening for input on the connection
+                if s in outputs:
+                    outputs.remove(s)  #既然客户端都断开了，我就不用再给它返回数据了，所以这时候如果这个客户端的连接对象还在outputs列表中，就把它删掉
+                inputs.remove(s)    #inputs中也删除掉
+                s.close()           #把这个连接关闭掉
+ 
+                # Remove message queue
+                del message_queues[s]
+    # Handle outputs
+    for s in writable:
+        try:
+            next_msg = message_queues[s].get_nowait()
+        except queue.Empty:
+            # No messages waiting so stop checking for writability.
+            print('output queue for', s.getpeername(), 'is empty')
+            outputs.remove(s)
+        else:
+            print( 'sending "%s" to %s' % (next_msg, s.getpeername()))
+            s.send(next_msg)
+    # Handle "exceptional conditions"
+    for s in exceptional:
+        print('handling exceptional condition for', s.getpeername() )
+        # Stop listening for input on the connection
+        inputs.remove(s)
+        if s in outputs:
+            outputs.remove(s)
+        s.close()
+ 
+        # Remove message queue
+        del message_queues[s]
